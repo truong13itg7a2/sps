5af72e3b351442fc6eca3dbdc0cc059cd9aa4472
truong13itg7a2 <17130915@st.hcmuaf.edu.vn>
2025-09-20 20:18:09 +0700

    1 txts1

--- pj200925/src/main/java/edu/txts/pj200925/service/UserService.java
+++ pj200925/src/main/java/edu/txts/pj200925/service/UserService.java
@@ -1,4 +1,42 @@
 package edu.txts.pj200925.service;
 
+import edu.txts.pj200925.model.User;
+import edu.txts.pj200925.repository.UserRepo;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+//@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public class UserService {
+	@Autowired
+	UserRepo userRepo;
+	public List<User> getAllUser(){
+		return userRepo.findAll();
+	}
+
+	public User getUserById(long id){
+		return userRepo.findById(id).get();
+	}
+
+	public User addUser(User user){
+		return userRepo.save(user);
+	}
+
+	public User updateUser(long id, User user){
+		User u = userRepo.findById(id).get();
+		u.setName(user.getName());
+		u.setAge(user.getAge());
+		u.setIncome(user.getIncome());
+		return userRepo.save(user);
+	}
+
+	public void deleteUser(long id){
+		userRepo.deleteById(id);
+	}
 }
