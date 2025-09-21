ccc4012e65bd3a76b089711f9207044fd2c873fc
truong13itg7a2 <17130915@st.hcmuaf.edu.vn>
2025-09-21 15:52:18 +0700

    1txts4

---
+++ p210925/src/main/java/edu/txts/p210925/P210925Application.java
@@ -0,0 +1,13 @@
+package edu.txts.p210925;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P210925Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P210925Application.class, args);
+	}
+
+}
---
+++ p210925/src/test/java/edu/txts/p210925/P210925ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.txts.p210925;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P210925ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
