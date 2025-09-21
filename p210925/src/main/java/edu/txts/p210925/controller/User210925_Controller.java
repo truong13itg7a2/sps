ecb55678616ec26757bf216a4e94b48a11bd1120
truong13itg7a2 <17130915@st.hcmuaf.edu.vn>
2025-09-21 17:01:23 +0700

    add cmt2

--- p210925/src/main/java/edu/txts/p210925/controller/User210925_Controller.java
+++ p210925/src/main/java/edu/txts/p210925/controller/User210925_Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //return user1
+//cmt2
 
 @RestController
 @RequestMapping({"/users"})
