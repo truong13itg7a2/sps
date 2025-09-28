4c7a9cf0e73731a310882fca5333a38113da772f
truong13itg7a2 <17130915@st.hcmuaf.edu.vn>
2025-09-28 21:58:30 +0700

    create spsfdsd1

---
+++ spsfdsd1/src/main/java/edu/txts/spsfdsd1/Spsfdsd1Application.java
@@ -0,0 +1,13 @@
+package edu.txts.spsfdsd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spsfdsd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spsfdsd1Application.class, args);
+	}
+
+}
---
+++ spsfdsd1/src/test/java/edu/txts/spsfdsd1/Spsfdsd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.txts.spsfdsd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spsfdsd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
