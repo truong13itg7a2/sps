e1a1d007857c2ecf460a672fb6f80fbc52a95533
truong13itg7a2 <17130915@st.hcmuaf.edu.vn>
2025-10-07 11:39:11 +0700

    0710251139

--- spsfdsd1/src/main/java/edu/txts/spsfdsd1/controller/UserD3_Controller.java
+++ spsfdsd1/src/main/java/edu/txts/spsfdsd1/controller/UserD3_Controller.java
@@ -1,9 +1,49 @@
 package edu.txts.spsfdsd1.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.txts.spsfdsd1.dto.request.UserD3_Request_Create;
+import edu.txts.spsfdsd1.dto.request.UserD3_Request_Update;
+import edu.txts.spsfdsd1.dto.response.UserD3_Response;
+import edu.txts.spsfdsd1.entity.UserD3;
+import edu.txts.spsfdsd1.service.impl.UserD3_Service_impl;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping({"/v3/users"})
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+
 public class UserD3_Controller {
+	UserD3_Service_impl userD3_Service;
+
+	@PostMapping
+	public UserD3_Response createUserD3(@RequestBody UserD3_Request_Create userD3_Request_Create) {
+		return userD3_Service.createUserD3(userD3_Request_Create);
+	}
+
+	@GetMapping
+	public List<UserD3> getAllUserD3() {
+		return userD3_Service.getAllUserD3v2();
+	}
+
+	@GetMapping({"/{id}"})
+	public UserD3_Response getUserD3(@PathVariable String id) {
+		return userD3_Service.getUserD3(id);
+	}
+
+	@PutMapping({"/{id}"})
+	public UserD3_Response updateUserD3(@PathVariable String id,
+	                                    @RequestBody UserD3_Request_Update userD3_Request_Update) {
+		return userD3_Service.updateUserD3(id, userD3_Request_Update);
+	}
+
+	@DeleteMapping({"/{id}"})
+	public void deleteUserD3(@PathVariable String id) {
+		userD3_Service.deleteUserD3(id);
+	}
+
 }
